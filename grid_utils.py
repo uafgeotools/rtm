d84ffe8f8835bd3cdf29b6fe6c1f50aaa74b1473
Liam Toney <ldtoney@alaska.edu>
2019-06-17 11:17:32 -0800

    Refactor utils.py; move grid creation function to grid_utils.py

---
+++ grid_utils.py
@@ -0,0 +1,129 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.io.img_tiles import Stamen
+import utm
+import warnings
+
+
+def define_grid(lon_0, lat_0, x_radius, y_radius, spacing, projected=False,
+                plot=False):
+    """
+    Define the spatial grid of trial source locations. Grid can be defined in
+    either a latitude/longitude or projected UTM (i.e., Cartesian) coordinate
+    system.
+
+    Args:
+        lon_0: [deg] Longitude of grid center
+        lat_0: [deg] Latitude of grid center
+        x_radius: [deg or m] Distance from lon_0 to edge of grid (i.e., radius)
+        y_radius: [deg or m] Distance from lat_0 to edge of grid (i.e., radius)
+        spacing: [deg or m] Desired grid spacing
+        projected: Boolean controlling whether a latitude/longitude or UTM grid
+                   is used. If True, a UTM grid is used and the units of
+                   x_radius, y_radius, and spacing are interpreted in meters
+                   instead of in degrees (default: False)
+        plot: Toggle plotting a preview of the grid for reference and
+              troubleshooting (default: False)
+    Returns:
+        grid_out: xarray.DataArray object containing the grid coordinates and
+                  metadata
+    """
+
+    # Make coordinate vectors
+    if projected:
+        x_0, y_0, zone_number, _ = utm.from_latlon(lat_0, lon_0)
+    else:
+        x_0, y_0, = lon_0, lat_0
+    # Using np.linspace() in favor of np.arange() due to precision advantages
+    x, dx = np.linspace(x_0 - x_radius, x_0 + x_radius,
+                        int(2 * x_radius / spacing) + 1, retstep=True)
+    y, dy = np.linspace(y_0 - y_radius, y_0 + y_radius,
+                        int(2 * y_radius / spacing) + 1, retstep=True)
+
+    # Basic grid checks
+    if dx != spacing:
+        warnings.warn(f'Requested spacing of {spacing} does not match '
+                      f'np.linspace()-returned x-axis spacing of {dx}.')
+    if dy != spacing:
+        warnings.warn(f'Requested spacing of {spacing} does not match '
+                      f'np.linspace()-returned y-axis spacing of {dy}.')
+    if not (x_0 in x and y_0 in y):
+        warnings.warn('(x_0, y_0) is not located in grid. Check for numerical '
+                      'precision problems (i.e., rounding error).')
+    if projected:
+        # Create list of grid corners
+        corners = dict(SW=(x.min(), y.min()),
+                       NW=(x.min(), y.max()),
+                       NE=(x.max(), y.max()),
+                       SE=(x.max(), y.min()))
+        for label, corner in corners.items():
+            # "Un-project" back to latitude/longitude
+            lat, lon = utm.to_latlon(*corner, zone_number, northern=lat_0 >= 0,
+                                     strict=False)
+            # "Re-project" to UTM
+            _, _, new_zone_number, _ = utm.from_latlon(lat, lon)
+            if new_zone_number != zone_number:
+                warnings.warn(f'{label} grid corner locates to UTM zone '
+                              f'{new_zone_number} instead of origin UTM zone '
+                              f'{zone_number}. Consider reducing grid extent '
+                              'or using an unprojected grid.')
+
+    # Create grid
+    data = np.full((y.size, x.size), np.nan)  # Initialize an array of NaNs
+    if projected:
+        coords = [('northing', y), ('easting', x)]
+        # Add the projection information to the grid metadata
+        proj_string = f'+proj=utm +zone={zone_number} +ellps=WGS84 +units=m ' \
+                      '+no_defs'
+        if lat_0 < 0:
+            proj_string += ' +south'
+        attrs = dict(proj=proj_string)
+    else:
+        coords = [('lat', y), ('lon', x)]
+        attrs = dict(proj=None)
+    grid_out = xr.DataArray(data, coords=coords, attrs=attrs)
+
+    # Plot grid preview, if specified
+    if plot:
+        if projected:
+            proj = ccrs.UTM(zone=zone_number, southern_hemisphere=lat_0 < 0)
+            transform = proj
+        else:
+            # This is a good projection to use since it preserves area
+            proj = ccrs.AlbersEqualArea(central_longitude=lon_0,
+                                        central_latitude=lat_0,
+                                        standard_parallels=(y.min(), y.max()))
+            transform = ccrs.PlateCarree()
+
+        fig, ax = plt.subplots(figsize=(10, 10),
+                               subplot_kw=dict(projection=proj))
+
+        # Since projected grids cover less area and may not include coastlines,
+        # use a background image to provide geographical context (can be slow)
+        if projected:
+            zoom_level = 12
+            ax.add_image(Stamen(style='terrain-background'), zoom_level)
+
+        # Since unprojected grids have regional/global extent, just show the
+        # coastlines
+        else:
+            scale = '10m'
+            feature = cfeature.LAND.with_scale(scale)
+            ax.add_feature(feature, facecolor=cfeature.COLORS['land'],
+                           edgecolor='black')
+            ax.background_patch.set_facecolor(cfeature.COLORS['water'])
+
+        # Note that trial source locations are at the CENTER of each plotted
+        # grid box
+        grid_out.plot.pcolormesh(ax=ax, transform=transform,
+                                 edgecolor='black', add_colorbar=False)
+
+        # Plot the center of the grid
+        ax.scatter(lon_0, lat_0, color='red', transform=ccrs.Geodetic())
+
+        fig.show()
+
+    return grid_out
