78bdc38821f7d1029c9137f95bbaf21ba5d5c8bb
Liam Toney <ldtoney@alaska.edu>
2019-08-27 15:47:32 -0800

    Update example to July Spurr mass flow

--- example.py
+++ example.py
@@ -2,10 +2,10 @@
 
 from rtm import define_grid
 
-LON_0 = -153.0918  # [deg] Longitude of grid center
-LAT_0 = 60.0319    # [deg] Latitude of grid center
+LON_0 = -152.238  # [deg] Longitude of grid center
+LAT_0 = 61.296    # [deg] Latitude of grid center
 
-PROJECTED = True
+PROJECTED = False
 
 if PROJECTED:
     X_RADIUS = 250  # [m] E-W grid radius (half of grid "width")
@@ -13,9 +13,9 @@ if PROJECTED:
     SPACING = 50    # [m] Grid spacing
 
 else:
-    X_RADIUS = 2   # [deg] E-W grid radius (half of grid "width")
-    Y_RADIUS = 2   # [deg] N-S grid radius (half of grid "height")
-    SPACING = 0.5  # [deg] Grid spacing
+    X_RADIUS = 1   # [deg] E-W grid radius (half of grid "width")
+    Y_RADIUS = 1   # [deg] N-S grid radius (half of grid "height")
+    SPACING = 0.1  # [deg] Grid spacing
 
 grid = define_grid(lon_0=LON_0, lat_0=LAT_0, x_radius=X_RADIUS,
                    y_radius=Y_RADIUS, spacing=SPACING, projected=PROJECTED,
@@ -28,17 +28,17 @@ from obspy import UTCDateTime
 from rtm import gather_waveforms_bulk, process_waveforms
 
 # Start and end of time window containing (suspected) events
-STARTTIME = UTCDateTime('2019-06-20T23:55')
-ENDTIME = STARTTIME + 60*20
+STARTTIME = UTCDateTime('2019-07-15T16:10')
+ENDTIME = STARTTIME + 60*60
 
-MAX_RADIUS = 250        # [km] Radius within which to search for stations
+MAX_RADIUS = 500        # [km] Radius within which to search for stations
 
 FREQ_MIN = 0.5          # [Hz] Lower bandpass corner
 FREQ_MAX = 2            # [Hz] Upper bandpass corner
 
-DECIMATION_RATE = 0.05  # [Hz] New sampling rate to use for decimation
+DECIMATION_RATE = 0.1   # [Hz] New sampling rate to use for decimation
 
-SMOOTH_WIN = 120        # [s] Smoothing window duration
+SMOOTH_WIN = 60         # [s] Smoothing window duration
 
 # watc_credentials.json contains a single line with format ["user", "password"]
 with open('watc_credentials.json') as f:
@@ -46,8 +46,8 @@ with open('watc_credentials.json') as f:
 
 st = gather_waveforms_bulk(LON_0, LAT_0, MAX_RADIUS, STARTTIME, ENDTIME,
                            time_buffer=grid, remove_response=True,
-                           watc_username=None,
-                           watc_password=None)
+                           watc_username=watc_username,
+                           watc_password=watc_password)
 
 st_proc = process_waveforms(st, freqmin=FREQ_MIN, freqmax=FREQ_MAX,
                             envelope=True, smooth_win=SMOOTH_WIN,
@@ -60,7 +60,7 @@ from rtm import grid_search
 
 STACK_METHOD = 'sum'  # Choose either 'sum' or 'product'
 
-CELERITY = 300  # [m/s]
+CELERITY = 295  # [m/s]
 
 S, shifted_streams = grid_search(processed_st=st_proc, grid=grid,
                                  celerity=CELERITY, starttime=STARTTIME,
@@ -70,14 +70,14 @@ S, shifted_streams = grid_search(processed_st=st_proc, grid=grid,
 
 from rtm import plot_time_slice, plot_record_section, get_max_coordinates
 
-fig = plot_time_slice(S, st_proc, time_slice=None, label_stations=False,
-                      hires=False)
+fig = plot_time_slice(S, st_proc, time_slice=None, label_stations=True,
+                      hires=True)
 
 time_max, y_max, x_max = get_max_coordinates(S, unproject=S.UTM)
 
 fig = plot_record_section(st_proc, origin_time=time_max,
                           source_location=(y_max, x_max),
-                          plot_celerity=[S.celerity], label_waveforms=False)
+                          plot_celerity=[S.celerity], label_waveforms=True)
 
 #%% DEM sandbox
 
