1f257c609717643e28db7cab589d56513ce5b485
Liam Toney <ldtoney@alaska.edu>
2019-10-02 22:42:09 -0800

    Remove AVO JSON file stuff

--- rtm/__init__.py
+++ rtm/__init__.py
@@ -1,7 +1,3 @@
-"""
-Reverse time migration of infrasound signals.
-"""
-
 # -----------------------------------------------------------------------------
 # WARNING CONFIG
 # -----------------------------------------------------------------------------
@@ -29,29 +25,6 @@ warnings.formatwarning = _formatwarning
 # Clean up
 del _formatwarning
 del warnings
-
-# -----------------------------------------------------------------------------
-# LOAD AVO INFRASOUND STATION DATA
-# -----------------------------------------------------------------------------
-
-import os
-import json
-
-dirname = os.path.dirname(__file__)
-
-# Load AVO infrasound station calibration values (units are Pa/ct)
-with open(os.path.join(dirname, 'avo_json', 'avo_infra_calibs.json')) as f:
-    AVO_INFRA_CALIBS = json.load(f)
-
-# Load AVO infrasound station coordinates (elevation units are meters)
-with open(os.path.join(dirname, 'avo_json', 'avo_infra_coords.json')) as f:
-    AVO_INFRA_COORDS = json.load(f)
-
-# Clean up
-del f
-del dirname
-del json
-del os
 
 # -----------------------------------------------------------------------------
 # EXPOSE PUBLIC MODULES
