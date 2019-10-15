522437f91c9df9f71cbfb6b4e3237511ed65b068
Liam Toney <ldtoney@alaska.edu>
2019-10-15 15:37:36 -0800

    Update local example

--- example_local.py
+++ example_local.py
@@ -1,63 +1,64 @@
-#%% (1) Define search and source grids
+#%% (1) Define grid
 
 from rtm import define_grid, produce_dem
 
-# Change to DEM from https://opentopography.org/ at some point
-EXTERNAL_FILE = 'DEM_Union_UAV_161116_sm101.tif'
+"""
+To obtain the below file from OpenTopography, run the command
+
+    $ curl https://cloud.sdsc.edu/v1/AUTH_opentopography/hosted_data/OTDS.072019.4326.1/raster/DEM_WGS84.tif -o DEM_WGS84.tif
+    
+or simply paste the above URL in a web browser. Alternatively, specify None to
+automatically download and use 1 arc-second STRM data.
+"""
+EXTERNAL_FILE = 'DEM_WGS84.tif'
 
 LON_0 = 169.448212  # [deg] Longitude of grid center
-LAT_0 = -19.527908   # [deg] Latitude of grid center
+LAT_0 = -19.527908  # [deg] Latitude of grid center
 
-X_RADIUS = 500  # [m] E-W grid radius (half of grid "width")
-Y_RADIUS = 500  # [m] N-S grid radius (half of grid "height")
+X_RADIUS = 600  # [m] E-W grid radius (half of grid "width")
+Y_RADIUS = 650  # [m] N-S grid radius (half of grid "height")
 SPACING = 10    # [m] Grid spacing
 
-X_RADIUS_SOURCE = 1000  # [m] E-W grid radius (half of grid "width")
-Y_RADIUS_SOURCE = 1000  # [m] N-S grid radius (half of grid "height")
-SPACING_SOURCE = 10    # [m] Grid spacing
+grid = define_grid(lon_0=LON_0, lat_0=LAT_0, x_radius=X_RADIUS,
+                   y_radius=Y_RADIUS, spacing=SPACING, projected=True)
 
-search_grid = define_grid(lon_0=LON_0, lat_0=LAT_0, x_radius=X_RADIUS,
-                          y_radius=Y_RADIUS, spacing=SPACING, projected=True)
-
-search_dem = produce_dem(search_grid, external_file=EXTERNAL_FILE)
-
-source_grid = define_grid(lon_0=LON_0, lat_0=LAT_0, x_radius=X_RADIUS_SOURCE,
-                          y_radius=Y_RADIUS_SOURCE, spacing=SPACING_SOURCE,
-                          projected=True)
-
-source_dem = produce_dem(source_grid, external_file=EXTERNAL_FILE)
-
+dem = produce_dem(grid, external_file=EXTERNAL_FILE)
 
 #%% (2) Grab and process the data
 
-from obspy import UTCDateTime, Stream
+from obspy import UTCDateTime
 from waveform_collection import gather_waveforms
-from rtm import process_waveforms
+from rtm import process_waveforms, calculate_time_buffer
 
 # Start and end of time window containing (suspected) events
-STARTTIME = UTCDateTime('2016-07-30T05:22:48')
+STARTTIME = UTCDateTime('2016-07-30T05:22:45')
 ENDTIME = STARTTIME + 10
 
+# Data collection parameters
 SOURCE = 'IRIS'
 NETWORK = '3E'
 STATION = 'YIF?'
 LOCATION = '*'
 CHANNEL = '*'
 
-FREQ_MIN = .5          # [Hz] Lower bandpass corner
-FREQ_MAX = 10            # [Hz] Upper bandpass corner
+MAX_STATION_DIST = 0.8  # [km] Max. dist. from grid center to station (approx.)
+
+FREQ_MIN = 0.5  # [Hz] Lower bandpass corner
+FREQ_MAX = 10   # [Hz] Upper bandpass corner
 
 DECIMATION_RATE = 10  # [Hz] New sampling rate to use for decimation
 SMOOTH_WIN = 1        # [s] Smoothing window duration
 
+# Automatically determine appropriate time buffer in s
+time_buffer = calculate_time_buffer(grid, MAX_STATION_DIST)
+
 st = gather_waveforms(source=SOURCE, network=NETWORK, station=STATION,
                       location=LOCATION, channel=CHANNEL, starttime=STARTTIME,
-                      endtime=ENDTIME)
+                      endtime=ENDTIME, time_buffer=time_buffer)
 
 st_proc = process_waveforms(st, freqmin=FREQ_MIN, freqmax=FREQ_MAX,
                             envelope=True, smooth_win=SMOOTH_WIN,
-                            decimation_rate=DECIMATION_RATE, agc_params=None,
-                            normalize=True, plot_steps=False)
+                            decimation_rate=DECIMATION_RATE, normalize=True)
 
 #%% (3) Perform grid search
 
@@ -65,20 +66,19 @@ from rtm import grid_search
 
 STACK_METHOD = 'sum'  # Choose either 'sum' or 'product'
 TIME_METHOD = 'celerity'  # Choose either 'celerity' or 'fdtd'
-TIME_KWARGS = {'celerity': 343, 'dem': search_dem}
+TIME_KWARGS = {'celerity': 343, 'dem': dem}
 
-Scel = grid_search(processed_st=st_proc, grid=search_grid,
-                   time_method=TIME_METHOD, starttime=None, endtime=None,
-                   stack_method=STACK_METHOD, **TIME_KWARGS)
+S = grid_search(processed_st=st_proc, grid=grid, time_method=TIME_METHOD,
+                stack_method=STACK_METHOD, **TIME_KWARGS)
 
 #%% (4) Plot
 
 from rtm import plot_time_slice, plot_record_section, get_max_coordinates
 
-plot_time_slice(Scel, st_proc, label_stations=True, dem=source_dem)
+fig = plot_time_slice(S, st_proc, label_stations=True, dem=dem)
 
-time_max, y_max, x_max = get_max_coordinates(Scel, unproject=Scel.UTM)
+time_max, y_max, x_max = get_max_coordinates(S, unproject=S.UTM)
 
 plot_record_section(st_proc, origin_time=time_max,
                     source_location=(y_max, x_max),
-                    plot_celerity=Scel.celerity, label_waveforms=True)
+                    plot_celerity=S.celerity, label_waveforms=True)
