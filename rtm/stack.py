2a9e711a310c48486542c4f5d75ec2b23f5307cd
David Fee <dfee1@alaska.edu>
2019-11-01 08:21:02 -0800

    add get_peak_coordinates function

--- rtm/stack.py
+++ rtm/stack.py
@@ -1,6 +1,7 @@
 from obspy import UTCDateTime
 import numpy as np
 import utm
+from scipy.signal import find_peaks
 import warnings
 from . import RTMWarning
 
@@ -56,6 +57,56 @@ def get_max_coordinates(S, unproject=False):
             y_max, x_max = utm.to_latlon(x_max, y_max,
                                          zone_number=S.UTM['zone'],
                                          northern=not S.UTM['southern_hemisphere'])
+        # If the grid is already in lat/lon
+        else:
+            print('unproject=True is set but coordinates are already in '
+                  '(latitude, longitude). Doing nothing.')
+
+    return time_max, y_max, x_max
+
+def get_peak_coordinates(S, height, min_time, unproject=False):
+    """
+    Find the values of the coordinates corresponding to the maxima (peaks) in
+    a stack function S.  Optionally "unprojects" UTM coordinates to (latitude,
+    longitude) for projected grids.
+
+    Args:
+        S: xarray.DataArray containing the stack function S
+        height: Minimum peak height in stack function.
+        min_time: Minimum time (distance) between peaks in stack function [s].
+        unproject: If True and if S is a projected grid, unprojects the UTM
+                   coordinates to (latitude, longitude) (default: False)
+    Returns:
+        time_max: Time (UTCDateTime) corresponding to peaks in S
+        y_max: [deg lat. or m N] y-coordinates corresponding to peaks in S
+        x_max: [deg lon. or m E] x-coordinates corresponding to peaks in S
+    """
+
+    #create peak stack function over time
+    s_peak = S.max(axis=(1, 2)).data
+
+    peak_dt = (np.datetime64(S['time'][1].data) - np.datetime64(S['time'][0].data)) \
+    / np.timedelta64(1, 's')
+
+    #find peaks in stack function
+    peaks, props = find_peaks(s_peak, height, distance=min_time/peak_dt)
+    npeaks = len(peaks)
+
+    time_max = [UTCDateTime(S['time'][i].values.astype(str)) for i in peaks]
+    x_max = [S.where(S[i] == S[i].max(), drop=True).squeeze()['x'].values.tolist() \
+             for i in peaks]
+    y_max = [S.where(S[i] == S[i].max(), drop=True).squeeze()['y'].values.tolist() \
+             for i in peaks]
+
+    if unproject:
+        # If the grid is projected
+        if S.UTM:
+            print('Unprojecting coordinates from UTM to (latitude, longitude).')
+            for i in range(0, npeaks):
+                y_max[i], x_max[i] = utm.to_latlon(x_max[i], y_max[i],
+                     zone_number=S.UTM['zone'],
+                     northern=not S.UTM['southern_hemisphere'])
+
         # If the grid is already in lat/lon
         else:
             print('unproject=True is set but coordinates are already in '
