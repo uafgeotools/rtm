e0d398e4e219b99c59d9f2dc1ab04a87da9c2b45
David Fee <dfee1@alaska.edu>
2019-11-05 08:56:24 -0900

    peak params not used for global_max

--- rtm/stack.py
+++ rtm/stack.py
@@ -5,19 +5,23 @@ import utm
 from scipy.signal import find_peaks
 from . import RTMWarning
 
-
-def get_peak_coordinates(S, height, min_time, global_max=True, unproject=False):
+def get_peak_coordinates(S, global_max=True, height=None, min_time=None,
+                         unproject=False):
     """
     Find the values of the coordinates corresponding to the maxima (peaks) in
-    a stack function S.  Optionally "unprojects" UTM coordinates to (latitude,
-    longitude) for projected grids.
+    a stack function S. Function will return all peaks above the "height" and
+    separated by greater than "min_time" in the stack function. Optionally
+    "unprojects" UTM coordinates to (latitude, longitude) for projected grids.
 
     Args:
         S: xarray.DataArray containing the stack function S
-        height: Minimum peak height in stack function.
-        min_time: Minimum time (distance) between peaks in stack function [s].
         global_max: Return the values for only the max of the stack function
                     (default: True)
+        height: Minimum threshold for the value of a detection (peak) in
+                the stack function S (default: None). Only used if
+                global_max=False.
+        min_time: Minimum time (distance) between peaks in stack function S [s]
+            (default: None). Only used if global_max=False.
         unproject: If True and if S is a projected grid, unprojects the UTM
                    coordinates to (latitude, longitude) (default: False)
     Returns:
@@ -29,21 +33,12 @@ def get_peak_coordinates(S, height, min_time, global_max=True, unproject=False):
     # Create peak stack function over time
     s_peak = S.max(axis=(1, 2)).data
 
-    peak_dt = (np.datetime64(S['time'][1].data) -
-               np.datetime64(S['time'][0].data)) / np.timedelta64(1, 's')
-
-    # Find peaks in stack function
-    peaks, props = find_peaks(s_peak, height, distance=min_time/peak_dt)
-    npeaks = len(peaks)
-
-    print(f'Found {npeaks} peaks in stack for height = {height:.1f} and '
-          f'min_time = {min_time/peak_dt:.1f} s.')
-
-    # Return just the global max. check for multiple maxima along each dimension
-    # and across the stack function
+    # Return just the global max or desired peaks. Check for multiple maxima
+    # along each dimension and across the stack function
     if global_max:
         print('Returning just global max!')
 
+        #get global max
         stack_maximum = S.where(S == S.max(), drop=True)
 
         # Warn if we have multiple maxima along any dimension
@@ -53,6 +48,10 @@ def get_peak_coordinates(S, height, min_time, global_max=True, unproject=False):
                 warnings.warn(f'Multiple maxima ({num_dim_maxima}) present in S '
                               f'along the {dim} dimension.', RTMWarning)
 
+        # return all peaks
+        peaks, props = find_peaks(s_peak, (None, None))
+
+        # check for multiple global maxima
         max_args = np.argwhere(props['peak_heights'] ==
                                np.amax(props['peak_heights']))
 
@@ -62,7 +61,20 @@ def get_peak_coordinates(S, height, min_time, global_max=True, unproject=False):
                           'in S. Using first occurrence.', RTMWarning)
 
         peaks = np.array(peaks[max_args])[0]
+    else:
+
+        if (height is None) or (min_time is None):
+            raise ValueError('height and min_time must be supplied for ' \
+                             'peak detection when global_max=False!')
+
+        peak_dt = (np.datetime64(S['time'][1].data) -
+               np.datetime64(S['time'][0].data)) / np.timedelta64(1, 's')
+
+        peaks, props = find_peaks (s_peak, height, distance=min_time/peak_dt)
+
         npeaks = len(peaks)
+        print(f'Found {npeaks} peaks in stack for height > {height:.1f} and '
+              f'min_time > {min_time/peak_dt:.1f} s.')
 
     time_max = [UTCDateTime(S['time'][i].values.astype(str)) for i in peaks]
     x_max = [S.where(S[i] == S[i].max(), drop=True).squeeze()['x'].values.tolist()
