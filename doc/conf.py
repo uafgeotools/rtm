a30e7794b55402c1e8962964f55c17c5a6088438
Liam Toney <ldtoney@alaska.edu>
2020-01-20 13:28:17 -0900

    Add Sphinx stuff

---
+++ doc/conf.py
@@ -0,0 +1,19 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../rtm'))
+
+project = 'rtm'
+
+copyright = '2020, David Fee & Liam Toney'
+
+author = 'David Fee & Liam Toney'
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon',
+              'sphinx.ext.intersphinx', 'recommonmark', 'sphinx.ext.viewcode']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
---
+++ doc/index.rst
@@ -0,0 +1,14 @@
+rtm
+===
+
+A Python package for reverse time migration of infrasound signals
+
+.. toctree::
+   :caption: README
+
+   README.md
+
+.. toctree::
+   :caption: API Reference
+
+   api/modules.rst
