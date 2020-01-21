1e002a092d1fd09cf9b5dcd074dff77741a912b0
Liam Toney <ldtoney@alaska.edu>
2020-01-20 16:47:06 -0900

    First attempt at building using setuptools
    These changes reflect the same approach as outlined in this issue:
    https://github.com/readthedocs/readthedocs.org/issues/5512

--- doc/conf.py
+++ doc/conf.py
@@ -8,8 +8,12 @@ copyright = '2020, David Fee & Liam Toney'
 
 author = 'David Fee & Liam Toney'
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon',
-              'sphinx.ext.intersphinx', 'recommonmark', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.intersphinx',
+              'recommonmark',
+              'sphinx.ext.viewcode'
+              ]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -22,4 +26,13 @@ napoleon_numpy_docstring = False
 
 master_doc = 'index'
 
-autodoc_mock_imports = ['waveform_collection']
+autodoc_mock_imports = ['numpy',
+                        'matplotlib',
+                        'xarray',
+                        'cartopy',
+                        'osgeo',
+                        'obspy',
+                        'utm',
+                        'scipy',
+                        'mpl_toolkits'
+                        ]
